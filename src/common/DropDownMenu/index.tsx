eac76fd5c13d9ec1da7b1a165844e6d26400f576
thanasislerai <thanasisleraj@yahoo.com>
2021-06-28 22:00:35 +0300

    Add view profile button

--- src/common/DropDownMenu/index.tsx
+++ src/common/DropDownMenu/index.tsx
@@ -8,8 +8,11 @@ import {
   WithStyles,
   createStyles,
   Theme,
+  Divider,
 } from "@material-ui/core";
+import { fade } from "@material-ui/core/styles/colorManipulator";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import PersonIcon from "@material-ui/icons/Person";
 import { red } from "@material-ui/core/colors";
 import userServices from "../../services/userServices";
 import UserContext from "../../contexts/UserContext";
@@ -34,7 +37,18 @@ const DropDownMenu = ({ anchorEl, onClose, classes }: DropDownMenuProps) => {
       anchorEl={anchorEl}
       open={Boolean(anchorEl)}
       onClose={onClose}
+      classes={{
+        list: classes.dropdownList,
+        paper: classes.dropdownListWrapper,
+      }}
     >
+      <MenuItem button className={classes.menuItem}>
+        <ListItemIcon className={classes.menuItemIcon}>
+          <PersonIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>View Profile</ListItemText>
+      </MenuItem>
+      <Divider className={classes.divider} />
       <MenuItem button className={classes.menuItem} onClick={onUserSignOut}>
         <ListItemIcon className={classes.menuItemIcon}>
           <PowerSettingsNewIcon
@@ -51,6 +65,8 @@ const DropDownMenu = ({ anchorEl, onClose, classes }: DropDownMenuProps) => {
 const styles = (theme: Theme) =>
   createStyles({
     menuItem: {
+      paddingTop: theme.spacing(0.5),
+      paddingBottom: theme.spacing(0.5),
       "&:hover": {
         backgroundColor: theme.palette.primary.main,
       },
@@ -62,6 +78,19 @@ const styles = (theme: Theme) =>
 
     signOutIcon: {
       color: red[500],
+    },
+
+    dropdownListWrapper: {
+      border: `1px solid ${theme.palette.primary.main}`,
+    },
+
+    dropdownList: {
+      paddingTop: theme.spacing(0.5),
+      paddingBottom: theme.spacing(0.5),
+    },
+
+    divider: {
+      backgroundColor: fade(theme.palette.primary.main, 0.5),
     },
   });
 
