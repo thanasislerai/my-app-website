fc39b8a3602daa9259852ff442520e61f757f257
thanasislerai <thanasisleraj@yahoo.com>
2021-07-11 19:07:11 +0300

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+jest.mock("./routes/HomePage/Map", () => "MockMap");
+
+test("renders as expected", () => {
+  const { container } = render(<App />);
+  expect(container).toMatchSnapshot();
 });
