5f389ce289a4d151ead68e288b183e8245cd06b5
thanasislerai <thanasisleraj@yahoo.com>
2021-09-18 11:44:45 +0300

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,39 @@
 import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 
 import App from "./App";
+import { mockUser } from "./mocks/user";
+import { RootState } from "./store/configure";
+
+const mockStore = configureStore<RootState>();
 
 jest.mock("./routes/HomePage/Map", () => "MockMap");
 
-test("renders as expected", () => {
-  const { container } = render(<App />);
-  expect(container).toMatchSnapshot();
+describe("App", () => {
+  let element: HTMLElement;
+  beforeEach(() => {
+    const store = mockStore({
+      theme: {
+        type: "dark",
+      },
+      user: {
+        userInfo: mockUser,
+        loading: false,
+        error: null,
+      },
+    });
+
+    store.dispatch = jest.fn();
+
+    element = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    ).container;
+  });
+
+  it("renders as expected", () => {
+    expect(element).toMatchSnapshot();
+  });
 });
---
+++ src/mocks/user.ts
@@ -0,0 +1,7 @@
+import { User } from "../store/user/types";
+
+export const mockUser: User = {
+  email: "test@mail.com",
+  token: "SoMeRaNdOmStRiNg",
+  userName: "testuser",
+};
