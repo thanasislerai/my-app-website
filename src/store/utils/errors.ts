73194d9524fd31a16e7b1afa460cbcc27c4115d2
thanasislerai <thanasisleraj@yahoo.com>
2022-03-13 01:25:39 +0200

    Update `lodash` imports

--- src/store/utils/errors.ts
+++ src/store/utils/errors.ts
@@ -1,5 +1,5 @@
 import { AxiosError } from "axios";
-import { get } from "lodash";
+import get from "lodash/get";
 
 export const getAxiosErrorMessage = (err: unknown) => {
   const error = err as AxiosError;
